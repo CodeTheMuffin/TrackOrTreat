25ffd21c264f3565b84b0ff8693b1eb60e5bb800
CodeTheMuffin <rt062295@gmail.com>
2018-10-27 10:14:33 -0400

    Updated main script and included basic example html page

--- src/the_main_script.js
+++ src/the_main_script.js
@@ -2,3 +2,15 @@ var platform = new H.service.Platform({
   'app_id': 'tzZGI6JfskLhH9t9r0I7',
   'app_code': 'vb_5ylZiTE51r_xX7D9MwQ'
 });
+
+//Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+  document.getElementById('mapContainer'),
+  defaultLayers.normal.map,
+  {
+    zoom: 10,
+    center: { lat: 52.5, lng: 13.4 }
+  });
